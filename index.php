25d81ef9a7e65a94623e3e19a6e036c630533cc3
Thiago Paulino <thiagop1985@gmail.com>
2015-02-16 17:03:45 -0200

    test
    test

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
 <?php
 echo gethostname(); // may output e.g,: sandie
 
+// Example use of getenv()
+$ip = getenv('REMOTE_ADDR');
+
+// Or simply use a Superglobal ($_SERVER or $_ENV)
+$ip = $_SERVER['REMOTE_ADDR'];
+
+
 ?>
