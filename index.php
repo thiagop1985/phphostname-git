707a44f1ac3303d4d8083ffd99377f10e90387ea
Thiago Paulino <thiagop1985@gmail.com>
2015-03-03 16:56:48 -0300

    Update index.php
    teste

--- index.php
+++ index.php
@@ -1,11 +1,4 @@
 <?php
-echo gethostname(); // may output e.g,: sandie
-
-// Example use of getenv()
-$ip = getenv('REMOTE_ADDR');
-
-// Or simply use a Superglobal ($_SERVER or $_ENV)
-$ip = $_SERVER['REMOTE_ADDR'];
-
+echo gethostname(); 
 
 ?>
